3158b17a1bfebe63c87d9ccbab9090d518d76d26
Pranav Temkar <pranavtemkar@gmail.com>
2022-04-11 17:22:38 +0000

    santoni: VoltageOS bringup
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- voltage_santoni.mk
+++ voltage_santoni.mk
@@ -22,37 +22,30 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 # Inherit from santoni device
 $(call inherit-product, device/xiaomi/santoni/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common VoltageOS stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-# Prebuilt apps
-$(call inherit-product-if-exists, vendor/miuicamera/config.mk)
+# Official-ify
+VOLTAGE_BUILD_TYPE := OFFICIAL
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1280
 
 PRODUCT_DEVICE := santoni
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4X
-PRODUCT_NAME := lineage_santoni
+PRODUCT_NAME := voltage_santoni
 BOARD_VENDOR := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
-
-# Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Touch HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.xiaomi_8937
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
-# Boot animation
-TARGET_BOOT_ANIMATION_RES := 720
-	
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
